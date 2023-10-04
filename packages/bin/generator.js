cebe1025a3548cb5af2c599b0702c8278bc3e797
h00ns <jihoon.hwang@imweb.me>
2023-10-04 21:54:32 +0900

    [refactor] generator 수정

--- packages/bin/generator.js
+++ packages/bin/generator.js
@@ -2,7 +2,7 @@
 const { execSync } = require("child_process");
 const path = require("path");
 const fs = require("fs");
-const { moveSync, removeSync } = require("fs-extra");
+const { removeSync, copySync } = require("fs-extra");
 
 // project-name 미입력
 if (process.argv.length < 3) {
@@ -13,16 +13,17 @@ if (process.argv.length < 3) {
 
 const projectName = process.argv[2];
 const currentPath = process.cwd();
-const parentPath = path.resolve(currentPath, "..");
+const isCurrentPathProject = projectName === ".";
 
-const tempPath = path.join(parentPath, "temp");
-const projectPath =
-  projectName === "." ? currentPath : path.join(currentPath, projectName);
+const tempPath = path.join(currentPath, "temp");
+const projectPath = isCurrentPathProject
+  ? currentPath
+  : path.join(currentPath, projectName);
 
 const GIT_REPO = "https://github.com/h00ns/create-react-hoon.git";
 
 // project-name 입력시
-if (projectName !== ".") {
+if (!isCurrentPathProject) {
   try {
     fs.mkdirSync(projectPath);
   } catch (err) {
@@ -44,17 +45,18 @@ async function main() {
     console.log("[INFO]: Downloading create-react-hoon...");
     execSync(`git clone ${GIT_REPO} ${tempPath}`);
 
-    console.log("[INFO]: Copying files...");
     // 임시 폴더에서 react-boilerplate만 복사
-    moveSync(`${tempPath}/react-boilerplate`, `${projectPath}`, {
-      overwrite: true,
-    });
+    console.log("[INFO]: Copying files...");
+    copySync(`${tempPath}/react-boilerplate`, projectPath);
+
     // 임시 폴더 삭제
     removeSync(tempPath);
 
     // 현재 경로 이동
     console.log("[INFO]: Moving into directory...");
-    process.chdir(projectPath);
+    if (!isCurrentPathProject) {
+      process.chdir(projectPath);
+    }
 
     // 의존성 설치
     console.log("[INFO]: install dependencies...");
